41f7f1421f57eb6a4cd0da2305929398befb9a4e
Andrew Barbanera <andrew.barbanera@wsmcorp.com>
2021-04-23 20:25:12 -0600

    main: initial commit

---
+++ src/main/groovy/com/inventory/reactive/ReactiveApplication.groovy
@@ -0,0 +1,13 @@
+package com.inventory.reactive
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReactiveApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication, args)
+	}
+
+}
